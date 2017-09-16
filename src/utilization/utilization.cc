a79cec60aa83f52152d3e1982709cde9f804a05d
Alexander <reisner.alex93@gmail.com>
2017-09-16 16:24:07 +0200

    Beautify loop output of utilization

--- src/utilization/utilization.cc
+++ src/utilization/utilization.cc
@@ -38,7 +38,9 @@ namespace Utilization{
 			util1=100-(tmp1-prev1)/(10*timestamp);
 			util2=100-(tmp2-prev2)/(10*timestamp);
 			util3=100-(tmp3-prev3)/(10*timestamp);
-			//Genode::printf("util: %d %d %d %d\n",util0,util1,util2,util3);
+
+			Genode::printf("utilization core0: %d core1: %d\n",util0,util1);
+
 			prev0=tmp0;
 			prev1=tmp1;
 			prev2=tmp2;
